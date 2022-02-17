32c18e3f433c0a3be1301ec8ed5948e450a92803
Ruslan Khyurri <ruslan.khyurri@gmail.com>
2022-02-17 12:48:40 +0300

    Initial Commit

---
+++ minesweeper/minesweeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  minesweeperApp.swift
+//  minesweeper
+//
+//  Created by Ruslan Khyurri on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct minesweeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
